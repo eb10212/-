756234a0097c3b702494a462e6e1e048f8c3adef
eb10212 <66315646+eb10212@users.noreply.github.com>
2020-07-18 11:35:48 +0800

    Update movie_project_add.py
    修改評論欄位(刪除)
    修改評論人數爬取矩陣[-1]
    新增的csv放在add3.csv

--- movie_project_add.py
+++ movie_project_add.py
@@ -6,9 +6,9 @@ import csv
 data=pd.read_csv('./movie_copy.csv')
 id_list=list(data['電影ID'])
 # id_list=[10345,10227]
-new_columns=['電影ID','滿意度','投票人數','評論人數','評論']
+new_columns=['電影ID','滿意度','投票人數','評論人數']
 df=pd.DataFrame(columns=new_columns)
-df.to_csv('./movie_add2.csv',index=False,encoding='utf-8-sig')
+df.to_csv('./movie_add3.csv',index=False,encoding='utf-8-sig')
 
 for id in id_list:
     add_column_data = []
@@ -35,42 +35,42 @@ for id in id_list:
 
     #評論人數-people
     try:
-        people_soup=soup.select('div[class="title_num"]')[4].text
+        people_soup=soup.select('div[class="title_num"]')[-1].text
         # print(people_soup)
         people=int(people_soup.strip('共').strip('則'))
         # print(type(people))
         # print(people)
         people_page=(people//10)+1
         # print(people)
-    # 評論-comment_list
-        page = 1
-        comment_list = []
-        while page <= people_page:
-            try:
-                # print(page)
-                url = 'https://movies.yahoo.com.tw/movieinfo_review.html/id={}?page={}'.format(id, page)
-                res = requests.get(url)
-                soup = BeautifulSoup(res.text, 'html.parser')
-
-                comment_soup = soup.select('ul[class="usercom_list"] span')
-                # print(type(comment_soup[0].text))
-                for comment in comment_soup:
-                    if comment.text == '':
-                        continue
-                    else:
-                        comment_list.append(comment.text)
-                page += 1
-            except IndexError:
-                page += 1
-                continue
+    # # 評論-comment_list
+    #     page = 1
+    #     comment_list = []
+    #     while page <= people_page:
+    #         try:
+    #             # print(page)
+    #             url = 'https://movies.yahoo.com.tw/movieinfo_review.html/id={}?page={}'.format(id, page)
+    #             res = requests.get(url)
+    #             soup = BeautifulSoup(res.text, 'html.parser')
+    #
+    #             comment_soup = soup.select('ul[class="usercom_list"] span')
+    #             # print(type(comment_soup[0].text))
+    #             for comment in comment_soup:
+    #                 if comment.text == '':
+    #                     continue
+    #                 else:
+    #                     comment_list.append(comment.text)
+    #             page += 1
+    #         except IndexError:
+    #             page += 1
+    #             continue
 
     except IndexError:
         people ='none'
         comment_list='none'
 
-    add_column = [id, satisfaction, vote, people, comment_list]
+    add_column = [id, satisfaction, vote, people]
 
-    with open('./movie_add2.csv', 'a', newline='', encoding='utf-8') as csvfile:
+    with open('./movie_add3.csv', 'a', newline='', encoding='utf-8') as csvfile:
         rows = csv.writer(csvfile)
         rows.writerow(add_column)
 
